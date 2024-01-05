59b78e595dd144fe02a99f79af13482e2a41b78d
SURESH VJ <vijjanaramasuresh@gmail.com>
2024-01-05 23:00:03 +0530

    test json

--- MeticulousStatsReporter.py
+++ MeticulousStatsReporter.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import json
 import os
 
-json_val = os.getenv("feed_json")
-print(f"MeticulousStatsReporter  {json_val}")
+
+json_path = os.getenv("feed_json")
+print(f"MeticulousStatsReporter  {json_path}")
+
+with open(json_path, 'r') as json_file:
+    data = json.load(json_file)
