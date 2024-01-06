f6d4bc1c73d98669b0174a1947e729b3bd11d154
SURESH VJ <vijjanaramasuresh@gmail.com>
2024-01-06 11:37:28 +0530

    oops setup

--- StatModInsighter.py
+++ StatModInsighter.py
@@ -2,8 +2,10 @@ import os
 from MeticulousStatsReporter import MeticulousStatsReporter as msr
 
 def stat_mod_insighter_start():
+    print("Pipeline running started.")
     github_raw_url = os.getenv("feed_json")
     smi_obj = msr(github_raw_url)
+    print("Pipeline running succeeded.")
     pass
 
 
